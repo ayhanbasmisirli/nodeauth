065ee20f17d5df4d1673bba8cdaeb6650e3f791e
ayhan basmisirli <ayhan.basmisirli@gmail.com>
2015-10-12 17:02:42 +0300

    register area added model addedd

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //Handle File Uploads
-var upload = multer({ dest: 'uploads/' });
+// var upload = multer({ dest: './uploads' });
 
-
+app.use(multer({dest:'./uploads'}));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
