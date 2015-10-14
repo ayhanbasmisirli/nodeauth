f236859c7e9c852261991315d173e60ee1218756
ayhan basmisirli <ayhan.basmisirli@gmail.com>
2015-10-14 11:58:43 +0300

    login and session set

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var User = require('../models/user');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -87,6 +89,41 @@ router.post('/register',function(req, res, next){
 		res.redirect('/');
 	}
 });
+passport.serializeUser(function(user ,done){
+	done(null,user.id);
+});
+passport.deserializeUser(function(id ,done){
+	User.getUserById(id,function(err,user){
+		done(err,user);
+	});
+});
+passport.use(new LocalStrategy(
+	function(username,password,done){
+		User.getUserByUsername(username,function(err,user){
+			if(err) throw err;
+			if(!user){
+				console.log('Unknow username');
+				return done(null ,false,{message:'Unknow User'});	
+			}
+		 User.comparePassword(password,user.password,function(err,isMatch){
+		 	if(err) throw err;
+		 	if(isMatch){ return done(null,user);}
+		 	else{console.log('invalid password');
+		 		return done(null,false,{message:'Invalid Password'});
 
+		 	}
+
+		 	});
+
+		});
+	}
+));
+
+
+router.post('/login',passport.authenticate('local',{failureRedirect:'/users/login',failureFlash:'invalid username or password'}),function(req,res){
+	console.log('authentication Successful');
+	req.flash ('success','You are logged in');
+	res.redirect('/');
+});
 
 module.exports = router;
